b6de824ae195f0372744076763bf0900c733d8dc
tsmith90 <tsmith90@my.westga.edu>
2021-03-19 11:00:24 +0800

    Added a DAL file and DBConnection class.

---
+++ DAL/DBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace ClinicSupport.DAL
+{
+    class DBConnection
+    {
+        /// <summary>
+        /// Method to establish a connection to the TechSupport DB
+        /// </summary>
+        /// <returns>The connection to the database</returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=cs-6232-g5;" +
+                "Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
