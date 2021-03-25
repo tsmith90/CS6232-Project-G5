96f89e944eaff3bb36fb53a9350478193d69a288
tsmith90 <tsmith90@my.westga.edu>
2021-03-25 19:20:58 +0800

    Added Visit model class.

---
+++ Model/Visit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicSupport.Model
+{
+    /// <summary>
+    /// Class to create and populate a Visit object from the DB
+    /// </summary>
+    class Visit
+    {
+        public int PatientID { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int NurseID { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public int Systolic { get; set; }
+
+        public int Diastolic { get; set; }
+
+        public decimal Temperature { get; set; }
+
+        public int Pulse { get; set; }
+
+        public string Symptoms { get; set; }
+
+        public string InitialDiagnosis { get; set; }
+
+        public string FinalDiagnosis { get; set; }
+    }
+}
