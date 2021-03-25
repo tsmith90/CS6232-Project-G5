6cca38638f4997701cfc15cb50f76c3a008e8705
tsmith90 <tsmith90@my.westga.edu>
2021-03-25 19:06:02 +0800

    Added Individual model class.

---
+++ Model/Individual.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClinicSupport.Model
+{
+    /// <summary>
+    /// Class to create and populate an Individual object from the DB
+    /// </summary>
+    class Individual
+    {
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int ZipCode { get; set; } 
+
+        public int PhoneNumber { get; set; }
+    }
+}
