a26eb991011d4958c56a6435d20e2470e4982412
tsmith90 <tsmith90@my.westga.edu>
2021-03-25 19:26:50 +0800

    Added Patient model class.

---
+++ Model/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicSupport.Model
+{
+    /// <summary>
+    /// Class to create and populate a Patient object from the DB
+    /// </summary>
+    class Patient
+    {
+        public int PatientID { get; set; }
+
+        public int IndividualID { get; set; }
+    }
+}
