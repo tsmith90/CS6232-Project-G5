ed490e065addc7346b527379a978f2f0042f573f
tsmith90 <tsmith90@my.westga.edu>
2021-03-25 19:00:09 +0800

    Added Appointment model class.

---
+++ Model/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClinicSupport.Model
+{
+    /// <summary>
+    /// Class to create and populate an Appointment object from the DB
+    /// </summary>
+    class Appointment
+    {
+        public int PatientID { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public int DoctorID { get; set; }
+    }
+}
