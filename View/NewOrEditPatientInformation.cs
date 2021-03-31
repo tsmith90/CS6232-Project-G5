7a5b2e5130a043a7f65fc827f9be94831170b77c
leecodya <lee.codya@gmail.com>
2021-03-31 08:46:18 -0500

    added comment to the new or edit patient info class

--- View/NewOrEditPatientInformation.cs
+++ View/NewOrEditPatientInformation.cs
@@ -2,6 +2,10 @@
 
 namespace ClinicSupport.View
 {
+    /// <summary>
+    /// This form is intended to be the way for the user to either create a new patient or edit an existing patient,
+    /// depending on which button is selected (if its even possible)
+    /// </summary>
     public partial class NewOrEditPatientInformation : Form
     {
         public NewOrEditPatientInformation()
