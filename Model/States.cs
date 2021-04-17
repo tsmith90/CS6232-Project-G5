a6a63501191f36250f3a1858bafe90f0eb8c1fe9
tsmith90 <tsmith90@my.westga.edu>
2021-04-17 19:59:07 +0800

    Moved repeated states list into a model class for easy updates.

---
+++ Model/States.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicSupport.Model
+{
+    public class States
+    {
+        Dictionary<string, string> statesList;
+
+        public States()
+        {
+            statesList = new Dictionary<string, string>();
+            SetStates();
+        }
+
+        public Dictionary<string, string> SetStates()
+        {
+            statesList = new Dictionary<string, string>();
+
+
+            statesList.Add("Alabama", "AL");
+            statesList.Add("Alaska", "AK");
+            statesList.Add("America Samoa", "AS");
+            statesList.Add("Arizona", "AZ");
+            statesList.Add("Arkansas", "AR");
+            statesList.Add("California", "CA");
+            statesList.Add("Colorado", "CO");
+            statesList.Add("Connecticut", "CT");
+            statesList.Add("Delaware", "DE");
+            statesList.Add("District of Columbia", "DC");
+            statesList.Add("Florida", "FL");
+            statesList.Add("Georgia", "GA");
+            statesList.Add("Guam", "GU");
+            statesList.Add("Hawaii", "HI");
+            statesList.Add("Idaho", "NurseID");
+            statesList.Add("Illinois", "IL");
+            statesList.Add("Indiana", "IN");
+            statesList.Add("Iowa", "IA");
+            statesList.Add("Kansas", "KS");
+            statesList.Add("Kentucky", "KY");
+            statesList.Add("Louisiana", "LA");
+            statesList.Add("Maine", "ME");
+            statesList.Add("Maryland", "MD");
+            statesList.Add("Massachusetts", "MA");
+            statesList.Add("Michigan", "MI");
+            statesList.Add("Minnesota", "MN");
+            statesList.Add("Mississippi", "MS");
+            statesList.Add("Missouri", "MO");
+            statesList.Add("Montana", "MT");
+            statesList.Add("Nebraska", "NE");
+            statesList.Add("Nevada", "NV");
+            statesList.Add("New Hampshire", "NH");
+            statesList.Add("New Jersey", "NJ");
+            statesList.Add("New Mexico", "NM");
+            statesList.Add("New York", "NY");
+            statesList.Add("North Carolina", "NC");
+            statesList.Add("North Dakota", "ND");
+            statesList.Add("Northern Mariana Islands", "MP");
+            statesList.Add("Ohio", "OH");
+            statesList.Add("Oklahoma", "OK");
+            statesList.Add("Oregon", "OR");
+            statesList.Add("Pennsylvania", "PA");
+            statesList.Add("Puerto Rico", "PR");
+            statesList.Add("Rhode Island", "RI");
+            statesList.Add("South Carolina", "SC");
+            statesList.Add("South Dakota", "SD");
+            statesList.Add("Tennessee", "TN");
+            statesList.Add("Texas", "TX");
+            statesList.Add("Utah", "UT");
+            statesList.Add("Vermont", "VT");
+            statesList.Add("Virginia", "VA");
+            statesList.Add("Virgin Islands", "VI");
+            statesList.Add("Washington", "WA");
+            statesList.Add("West Virginia", "WV");
+            statesList.Add("Wisconsin", "WI");
+            statesList.Add("Wyoming", "WY");
+
+            return statesList;
+        }
+    }
+}
--- UserControls/AddNurseUserControl.cs
+++ UserControls/AddNurseUserControl.cs
@@ -13,6 +13,8 @@ namespace ClinicSupport.UserControls
     {
 
         private readonly Dictionary<string, string> states;
+        private readonly States statesList;
+
 
         /// <summary>
         /// 0-parameter constructor for the AddNurseUserControl
@@ -20,7 +22,8 @@ namespace ClinicSupport.UserControls
         public AddNurseUserControl()
         {
             InitializeComponent();
-            states = new Dictionary<string, string>();
+            statesList = new States();
+            states = statesList.SetStates();
             SetStateList();
         }
 
@@ -50,63 +53,6 @@ namespace ClinicSupport.UserControls
 
         private void SetStateList()
         {
-            states.Add("Alabama", "AL");
-            states.Add("Alaska", "AK");
-            states.Add("America Samoa", "AS");
-            states.Add("Arizona", "AZ");
-            states.Add("Arkansas", "AR");
-            states.Add("California", "CA");
-            states.Add("Colorado", "CO");
-            states.Add("Connecticut", "CT");
-            states.Add("Delaware", "DE");
-            states.Add("District of Columbia", "DC");
-            states.Add("Florida", "FL");
-            states.Add("Georgia", "GA");
-            states.Add("Guam", "GU");
-            states.Add("Hawaii", "HI");
-            states.Add("Idaho", "NurseID");
-            states.Add("Illinois", "IL");
-            states.Add("Indiana", "IN");
-            states.Add("Iowa", "IA");
-            states.Add("Kansas", "KS");
-            states.Add("Kentucky", "KY");
-            states.Add("Louisiana", "LA");
-            states.Add("Maine", "ME");
-            states.Add("Maryland", "MD");
-            states.Add("Massachusetts", "MA");
-            states.Add("Michigan", "MI");
-            states.Add("Minnesota", "MN");
-            states.Add("Mississippi", "MS");
-            states.Add("Missouri", "MO");
-            states.Add("Montana", "MT");
-            states.Add("Nebraska", "NE");
-            states.Add("Nevada", "NV");
-            states.Add("New Hampshire", "NH");
-            states.Add("New Jersey", "NJ");
-            states.Add("New Mexico", "NM");
-            states.Add("New York", "NY");
-            states.Add("North Carolina", "NC");
-            states.Add("North Dakota", "ND");
-            states.Add("Northern Mariana Islands", "MP");
-            states.Add("Ohio", "OH");
-            states.Add("Oklahoma", "OK");
-            states.Add("Oregon", "OR");
-            states.Add("Pennsylvania", "PA");
-            states.Add("Puerto Rico", "PR");
-            states.Add("Rhode Island", "RI");
-            states.Add("South Carolina", "SC");
-            states.Add("South Dakota", "SD");
-            states.Add("Tennessee", "TN");
-            states.Add("Texas", "TX");
-            states.Add("Utah", "UT");
-            states.Add("Vermont", "VT");
-            states.Add("Virginia", "VA");
-            states.Add("Virgin Islands", "VI");
-            states.Add("Washington", "WA");
-            states.Add("West Virginia", "WV");
-            states.Add("Wisconsin", "WI");
-            states.Add("Wyoming", "WY");
-
             List<string> names = states.Keys.ToList();
 
             stateComboBox.DataSource = names;
