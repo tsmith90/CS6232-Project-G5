28fadf95ddf7e99062ab72c7cddd11034170d211
leecodya <lee.codya@gmail.com>
2021-04-19 20:27:17 -0500

    created TestController to interact with the TestDAL and added method to retrieve a list of Test objects from the TestDAL

---
+++ Controller/TestController.cs
@@ -0,0 +1,35 @@
+﻿using ClinicSupport.DAL;
+using ClinicSupport.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicSupport.Controller
+{
+    /// <summary>
+    /// Class to interact with the TestDAL
+    /// </summary>
+    class TestController
+    {
+        private readonly TestDAL testDAL;
+
+        /// <summary>
+        /// 0 parameter constructor for the TestController class
+        /// </summary>
+        public TestController()
+        {
+            testDAL = new TestDAL();
+        }
+
+        /// <summary>
+        /// Method to retrieve a list of Test objects from the TestDAL
+        /// </summary>
+        /// <returns>Returns a list of Test objects</returns>
+        public List<Test> GetTests()
+        {
+            return testDAL.GetTests();
+        }
+    }
+}
