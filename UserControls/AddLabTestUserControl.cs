f1aea2a5448ce6832a296e4fbfe40f3705327c02
leecodya <lee.codya@gmail.com>
2021-04-19 22:55:21 -0500

    created AddLabTestUserControl in order to handle the functionality of adding a lab test from the order test user control

---
+++ UserControls/AddLabTestUserControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace ClinicSupport.UserControls
+{
+    public partial class AddLabTestUserControl : UserControl
+    {
+        public AddLabTestUserControl()
+        {
+            InitializeComponent();
+            orderTestUserControl1.ForOrderingTests();
+        }
+    }
+}
